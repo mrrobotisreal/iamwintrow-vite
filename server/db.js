23849fcfe47cc7d105aeae97eca561843f89a6b7
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-11 21:21:13 -0600

    Finished GETComments func and POSTComment func in db.js

--- server/db.js
+++ server/db.js
@@ -8,12 +8,22 @@ const pool = new Pool({
 });
 
 function GETComments() {
-  return pool.query();
+  return pool.query(`SELECT * FROM comments`);
 }
 
-function POSTComment(comment) {
+function POSTComment(comment, poster) {
+  let date = Date.now();
+  let query = {
+    text: 'EXPLAIN ANALYZE INSERT INTO comments (poster, comment, date) VALUES ($1, $2, $3)',
+    values: [poster, comment, date]
+  };
+  pool.query(query);
+}
+
+function POSTVisitors(ip, date) {
   pool.query();
 }
 
 module.exports.GETComments = GETComments;
 module.exports.POSTComment = POSTComment;
+module.exports.POSTVisitors = POSTVisitors;
