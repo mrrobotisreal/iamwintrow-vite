d80bc1ee786593147b28ea2bff131fc8f08f5b42
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-11 20:51:41 -0600

    Added basic code for db file with postgres funcs.

--- server/db.js
+++ server/db.js
@@ -1,1 +1,19 @@
-// postgres
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: 'mitchwintrow',
+  password: '',
+  host: 'localhost',
+  port: 5432,
+  database: 'iamwintrow'
+});
+
+function GETComments() {
+  return pool.query();
+}
+
+function POSTComment(comment) {
+  pool.query();
+}
+
+module.exports.GETComments = GETComments;
+module.exports.POSTComment = POSTComment;
--- server/index.js
+++ server/index.js
@@ -2,16 +2,17 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const bodyParser = require('body-parser');
+const db = require('./db.js');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "../index.html")));
 
-// app.get('/', (req, res) => {
-//   console.log('home route');
-// });
+app.get('/comments', (req, res) => {
+  console.log('comments route');
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
