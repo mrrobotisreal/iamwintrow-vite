568cbea768f385e6bfc5f2cb2ac151c85f085efd
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-06-15 16:48:07 -0600

    Added basic Vanilla.jsx file.

---
+++ src/Blog/Vanilla/Vanilla.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Vanilla = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Vanilla;
